8888825f6274b30b9ab7536c892f70cbcea8ef4d
TomDongyangLi <90584973+TomDongyangLi@users.noreply.github.com>
2021-12-31 00:45:51 -0800

    Delete sparse directory

--- sparse/homogeneous/Generators/RHShat.m
+++
@@ -1,9 +0,0 @@
-function dotXhat = RHShat(t,X,d,N,C,psiLib)
-% creates the identified system that approximates dotX
-
-% form Theta = [f_psi_1 ... f_psi_n]
-Theta = generateTheta(t,X,psiLib,d,N);
-
-dotXhat = Theta*C;
-end
-
--- sparse/homogeneous/Utils/trajUnifNoiseAdditive.m
+++
@@ -1,6 +0,0 @@
-function trajData = trajUnifNoiseAdditive( trajData, sigma )
-
-% Adds additive gaussian noise of relative amplitude sigma to trajectories
-trajData = trajData+sigma*(randn(size(trajData)));
-
-return
